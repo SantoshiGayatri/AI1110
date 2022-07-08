2e61270156dca2b2b56547fef1a21909deaafb62
SantoshiGayatri <102791508+SantoshiGayatri@users.noreply.github.com>
2022-07-08 09:31:35 +0530

    Add files via upload

---
+++ Assign/codes/24.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *fp;
+    fp=fopen("gau.dat","r");
+    double sum,x,y=0;
+    int i=0;
+    while(fscanf(fp,"%lf",&x)!=-1){
+        sum+=x;i++;
+    }
+    fclose(fp);
+
+    y=sum/1000000;
+    printf("mean = %lf\n",y);
+
+    fp=fopen("gau.dat","r");
+    double sqr,z=0;
+    int j=0;
+    while(fscanf(fp,"%lf",&z)!=-1){
+        sqr+=(z-y)*(z-y);
+        j++;
+    }
+    fclose(fp);
+
+    double var=sqr/1000000;
+    printf("variance = %lf\n",var);
+    return 0;
+}
